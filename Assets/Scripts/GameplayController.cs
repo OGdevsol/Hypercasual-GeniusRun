c4bbba1f60f1d88f05d72891c8b1be126d7fc11f
Umar Amin <silentspy98@gmail.com>
2023-07-13 13:16:17 +0500

    New UI additions + Bug fixes + Vibration  + Quit method

--- Assets/Scripts/GameplayController.cs
+++ Assets/Scripts/GameplayController.cs
@@ -21,21 +21,21 @@ public class GameplayController : MonoBehaviour
     public GameObject onBikeSitParticle;
 
 
-  [HideInInspector]  public List<Spawnables>
+    [HideInInspector] public List<Spawnables>
         questionsInLevel; // List for current level questions to perform individual operations and win loss logic
 
     private HUDController hudController;
 
     #region AudioSources
+
     public AudioSource winSound;
     public AudioSource loseSound;
     public AudioSource onAnswerCorrectSound;
     public AudioSource onAnswerWrongSound;
     public AudioSource runningSound;
     public AudioSource explosionSound;
-    #endregion
-   
 
+    #endregion
 
 
     #region DataFlowControlVariables
@@ -43,8 +43,9 @@ public class GameplayController : MonoBehaviour
     private int currentLevelHolderVar;
     private int correctAnswers;
     private int wrongAnswers;
+
     #endregion
-    
+
     #region GameplayLogic
 
     private void Awake()
@@ -90,6 +91,7 @@ public class GameplayController : MonoBehaviour
         if (other.CompareTag("Correct"))
         {
             onAnswerCorrectSound.Play();
+            Handheld.Vibrate();
             Debug.Log("CorrectAnswer");
             Instantiate(onAnswerParticle, other.transform.position,
                 other.transform.rotation);
@@ -103,6 +105,8 @@ public class GameplayController : MonoBehaviour
         if (other.CompareTag("Wrong"))
         {
             onAnswerWrongSound.Play();
+            Handheld.Vibrate();
+
             Debug.Log("WrongAnswer");
             Instantiate(onAnswerParticle, other.transform.position,
                 other.transform.rotation);
@@ -139,9 +143,8 @@ public class GameplayController : MonoBehaviour
 
     private void CheckWinningCondition()
     {
-        
         float correctPerc = GetCorrectAnswers() / GetLevelQuestions() * 100;
-        
+
 
         switch (correctPerc)
         {
@@ -159,7 +162,7 @@ public class GameplayController : MonoBehaviour
     private void LevelWinSequence()
     {
         SetMaxLevelReached(); //Set only if level completed
-      
+
         Vector3 offSet = new Vector3(0, 0, 3);
         levelsData[GetCurrentLevel()].levelPlayer.SetActive(false);
         levelsData[GetCurrentLevel()].levelPlayer.transform.gameObject.GetComponentInParent<PathFollower>().speed = 25f;
@@ -169,24 +172,21 @@ public class GameplayController : MonoBehaviour
         DisableQuestions();
         runningSound.Stop();
         hudController.pauseButton.SetActive(false);
-        StartCoroutine(ActivatePanel(hudController.winPanel,winSound));
-        
-      
+        StartCoroutine(ActivatePanel(hudController.winPanel, winSound));
     }
 
     private void LevelFailSequence()
     {
-      
         levelsData[GetCurrentLevel()].levelPlayer.GetComponent<Animator>().Play("Sad");
         FindObjectOfType<PlayerMovement>().enabled = false;
         FindObjectOfType<PathFollower>().enabled = false;
         DisableQuestions();
         runningSound.Stop();
         hudController.pauseButton.SetActive(false);
-        StartCoroutine(ActivatePanel(hudController.losePanel,loseSound));
+        StartCoroutine(ActivatePanel(hudController.losePanel, loseSound));
     }
 
-    private IEnumerator ActivatePanel(GameObject panel , AudioSource audioType)
+    private IEnumerator ActivatePanel(GameObject panel, AudioSource audioType)
     {
         yield return new WaitForSeconds(4f);
         audioType.Play();
@@ -248,15 +248,14 @@ public class GameplayController : MonoBehaviour
     void SetMaxLevelReached()
     {
         Debug.Log("Current level is" + GetCurrentLevel() + "MAX Level is" + GetMaxLevelReached());
-        if (GetCurrentLevel()<9)
+        if (GetCurrentLevel() < 9)
         {
-            PlayerPrefs.SetInt("MaxLevel", GetCurrentLevel()+1);
+            PlayerPrefs.SetInt("MaxLevel", GetCurrentLevel() + 1);
         }
-        else if (GetCurrentLevel()>=9)
+        else if (GetCurrentLevel() >= 9)
         {
             PlayerPrefs.SetInt("MaxLevel", 9);
         }
-       
     }
 
     int GetMaxLevelReached()
@@ -289,7 +288,7 @@ public class GameplayController : MonoBehaviour
     {
         hudController.inGameloadingPanel.SetActive(true);
         yield return new WaitForSecondsRealtime(loadingTime);
-        if (GetCurrentLevel()<9)
+        if (GetCurrentLevel() < 9)
         {
             SetCurrentLevel(GetCurrentLevel() + 1);
         }
@@ -297,7 +296,7 @@ public class GameplayController : MonoBehaviour
         {
             SetCurrentLevel(0);
         }
-        
+
         SceneManager.LoadScene(1);
     }
 
@@ -307,64 +306,58 @@ public class GameplayController : MonoBehaviour
 
     public void volumebuttonclick()
     {
-        if (AudioListener.volume==0)
+        if (AudioListener.volume == 0)
         {
             AudioListener.volume = 1;
-            PlayerPrefs.SetInt("Volume",1);
+            PlayerPrefs.SetInt("Volume", 1);
             hudController.volToggle.GetComponent<Toggle>().isOn = true;
 
-         //   soundButton.GetComponent<Image>().sprite = UnMute;
+            //   soundButton.GetComponent<Image>().sprite = UnMute;
 
             //  UnMute.SetActive(true);
             //  Mute.SetActive(false);
-
         }
         else if (AudioListener.volume == 1)
         {
             AudioListener.volume = 0;
-            PlayerPrefs.SetInt("Volume",0);
+            PlayerPrefs.SetInt("Volume", 0);
             hudController.volToggle.GetComponent<Toggle>().isOn = false;
 
-           // soundButton.GetComponent<Image>().sprite = Mute;
+            // soundButton.GetComponent<Image>().sprite = Mute;
             //  Mute.SetActive(true);
             // UnMute.SetActive(false);
         }
-        
     }
 
     public void CheckVolumeSettings()
     {
-
         if (!PlayerPrefs.HasKey("Volume"))
         {
             AudioListener.volume = 1;
-            PlayerPrefs.SetInt("Volume",1);
+            PlayerPrefs.SetInt("Volume", 1);
             hudController.volToggle.GetComponent<Toggle>().isOn = true;
             //   soundButton.GetComponent<Image>().sprite = UnMute;
         }
         else
         {
-            if (PlayerPrefs.GetInt("Volume")==1)
+            if (PlayerPrefs.GetInt("Volume") == 1)
             {
                 AudioListener.volume = 1;
-                PlayerPrefs.SetInt("Volume",1);
+                PlayerPrefs.SetInt("Volume", 1);
                 hudController.volToggle.GetComponent<Toggle>().isOn = true;
 
-            //    soundButton.GetComponent<Image>().sprite = UnMute;
+                //    soundButton.GetComponent<Image>().sprite = UnMute;
             }
-            if (PlayerPrefs.GetInt("Volume")==0)
+
+            if (PlayerPrefs.GetInt("Volume") == 0)
             {
                 AudioListener.volume = 0;
                 hudController.volToggle.GetComponent<Toggle>().isOn = false;
 
-                PlayerPrefs.SetInt("Volume",0);
-             //   soundButton.GetComponent<Image>().sprite = Mute;
+                PlayerPrefs.SetInt("Volume", 0);
+                //   soundButton.GetComponent<Image>().sprite = Mute;
             }
         }
-
-
-       
-        
     }
 
     #endregion
--- Assets/Scripts/MainMenuUIController.cs
+++ Assets/Scripts/MainMenuUIController.cs
@@ -8,9 +8,12 @@ using UnityEngine.UI;
 public class MainMenuUIController : MonoBehaviour
 {
     [SerializeField] private List<GameObject> references;
-    [SerializeField] private GameObject loadingScreen;
+    
     [SerializeField] private Button[] levelButtons;
     [SerializeField] private GameObject mainMenuVolToggle;
+    [SerializeField] private GameObject loadingScreen;
+    [SerializeField] private Sprite unLockLevelBackground;
+    
 
     private void Awake()
     {
@@ -24,16 +27,17 @@ public class MainMenuUIController : MonoBehaviour
     {
         Application.OpenURL("https://lucidtecstudio.blogspot.com/2023/05/privacy-policy.html");
     }
+
     public void MoreGames()
     {
         Application.OpenURL("https://play.google.com/store/apps/developer?id=Lucidtec+Studio");
     }
+
     public void RateUs()
     {
         Application.OpenURL("https://play.google.com/store/apps/details?id=com.ls.Scattergories.runnergames.quizgames");
     }
-    
-    
+
 
     public void ActivateUIReference(int index)
     {
@@ -54,6 +58,7 @@ public class MainMenuUIController : MonoBehaviour
         {
             levelButtons[i].interactable = true;
             levelButtons[i].transform.GetChild(1).transform.gameObject.SetActive(false);
+            levelButtons[i].GetComponent<Image>().sprite = unLockLevelBackground;
         }
     }
 
@@ -75,29 +80,34 @@ public class MainMenuUIController : MonoBehaviour
         PlayerPrefs.SetInt("CurrentLevel", currentLevel);
     }
 
-    public int GetCurrentLevel(int currentLevel)
+    public int GetCurrentLevel()
     {
-        currentLevel = PlayerPrefs.GetInt("CurrentLevel");
-        return currentLevel;
+      
+        return  PlayerPrefs.GetInt("CurrentLevel");
     }
 
     int GetMaxLevelReached()
     {
         return PlayerPrefs.GetInt("MaxLevel");
     }
-    
+
+    public void OnCLickPlay()
+    {
+        LoadLevel(GetMaxLevelReached());
+    }
+
     public void volumebuttonclick()
     {
         switch (AudioListener.volume)
         {
             case 0:
                 AudioListener.volume = 1;
-                PlayerPrefs.SetInt("Volume",1);
+                PlayerPrefs.SetInt("Volume", 1);
                 mainMenuVolToggle.GetComponent<Toggle>().isOn = true;
                 break;
             case 1:
                 AudioListener.volume = 0;
-                PlayerPrefs.SetInt("Volume",0);
+                PlayerPrefs.SetInt("Volume", 0);
                 mainMenuVolToggle.GetComponent<Toggle>().isOn = false;
                 break;
         }
@@ -105,36 +115,32 @@ public class MainMenuUIController : MonoBehaviour
 
     public void CheckVolumeSettings()
     {
-
         if (!PlayerPrefs.HasKey("Volume"))
         {
             AudioListener.volume = 1;
-            PlayerPrefs.SetInt("Volume",1);
+            PlayerPrefs.SetInt("Volume", 1);
             mainMenuVolToggle.GetComponent<Toggle>().isOn = true;
-           
         }
         else
         {
-            if (PlayerPrefs.GetInt("Volume")==1)
+            if (PlayerPrefs.GetInt("Volume") == 1)
             {
                 AudioListener.volume = 1;
-                PlayerPrefs.SetInt("Volume",1);
+                PlayerPrefs.SetInt("Volume", 1);
                 mainMenuVolToggle.GetComponent<Toggle>().isOn = true;
-
-               
             }
-            if (PlayerPrefs.GetInt("Volume")==0)
+
+            if (PlayerPrefs.GetInt("Volume") == 0)
             {
                 AudioListener.volume = 0;
                 mainMenuVolToggle.GetComponent<Toggle>().isOn = false;
 
-                PlayerPrefs.SetInt("Volume",0);
-                
+                PlayerPrefs.SetInt("Volume", 0);
             }
         }
-
-
-       
-        
+    }
+    public  void QuitGame()
+    {
+        Application.Quit();
     }
 }
